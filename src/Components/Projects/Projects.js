7d4998371a4de15ba07059678a263f0c3cec2b5b
Antonio Barreiro <antonio.barreiro.dw@immune.institute>
2024-10-21 21:27:01 +0200

    Ajuste estilos

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,22 +1,48 @@
-import uniqid from 'uniqid'
-import { projects } from '../../portfolio'
-import ProjectContainer from '../ProjectContainer/ProjectContainer'
-import './Projects.css'
+import React, { useState } from 'react';
+import uniqid from 'uniqid';
+import { projects } from '../../portfolio';
+import ProjectContainer from '../ProjectContainer/ProjectContainer';
+import './Projects.css';
 
 const Projects = () => {
-  if (!projects.length) return null
+  const INITIAL_PROJECTS = 3; 
+  const [visibleProjects, setVisibleProjects] = useState(INITIAL_PROJECTS);
+
+  const handleShowMore = () => {
+    setVisibleProjects((prev) => (prev + 3 <= projects.length ? prev + 3 : projects.length)); 
+  };
+
+  const handleShowLess = () => {
+    setVisibleProjects(INITIAL_PROJECTS); 
+  };
+
+  if (!projects.length) return null;
 
   return (
     <section id='projects' className='section projects'>
       <h2 className='section__title'>Proyectos</h2>
 
       <div className='projects__grid'>
-        {projects.map((project) => (
+        {projects.slice(0, visibleProjects).map((project) => (
           <ProjectContainer key={uniqid()} project={project} />
         ))}
       </div>
-    </section>
-  )
-}
 
-export default Projects
+      <div className='projects__button-container'>
+        {visibleProjects < projects.length && (
+          <button className='projects__button' onClick={handleShowMore}>
+            Ver más
+          </button>
+        )}
+
+        {visibleProjects > INITIAL_PROJECTS && (
+          <button className='projects__button' onClick={handleShowLess}>
+            Ver menos
+          </button>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
